8fe20f68847a8bb2d76cd2fd1cf1c73e97c10e55
impratikspatil <impratikspatil@gmail.com>
2022-06-25 14:21:51 +0530

    .

--- app/src/main/java/com/example/imlicitintentkotlin/MainActivity.kt
+++ app/src/main/java/com/example/imlicitintentkotlin/MainActivity.kt
@@ -13,16 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
+        //this is the main code for imiplicit intent
         binding.btn.setOnClickListener {
             Intent(Intent.ACTION_GET_CONTENT).also {
                 it.type="image/*"
                 startActivityForResult(it,0)
+                // though this method is deprecated but I have used here
             }
         }
 
 
     }
 
+    //this method will receive data from device through intent and we can display that data wherever we want
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode== Activity.RESULT_OK && requestCode==0)
@@ -38,3 +42,5 @@ class MainActivity : AppCompatActivity() {
 //}
 
 //the use <layout> and use binding in activity so that we can fast easily access id
+
+///then used ActivityMainbinding and user binding onject anywhere we want
